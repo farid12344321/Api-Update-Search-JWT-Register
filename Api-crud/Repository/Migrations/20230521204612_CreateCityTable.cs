e66f6ae0082af2cb2bed3400d5da17f1163d83d5
Faridrab <faridrab@code.edu.az>
2023-05-22 02:21:30 +0400

    new crud

---
+++ Api-crud/Repository/Migrations/20230521204612_CreateCityTable.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Repository.Migrations
+{
+    public partial class CreateCityTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cities",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CountryId = table.Column<int>(type: "int", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    SoftDelete = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cities", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Cities_Countries_CountryId",
+                        column: x => x.CountryId,
+                        principalTable: "Countries",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Cities",
+                columns: new[] { "Id", "CountryId", "CreatedAt", "Name", "SoftDelete" },
+                values: new object[,]
+                {
+                    { 1, 1, new DateTime(2023, 5, 22, 0, 46, 12, 133, DateTimeKind.Local).AddTicks(5255), "Seki", false },
+                    { 2, 2, new DateTime(2023, 5, 22, 0, 46, 12, 133, DateTimeKind.Local).AddTicks(5259), "Istanbul", false },
+                    { 3, 3, new DateTime(2023, 5, 22, 0, 46, 12, 133, DateTimeKind.Local).AddTicks(5261), "London", false }
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2023, 5, 22, 0, 46, 12, 133, DateTimeKind.Local).AddTicks(5212));
+
+            migrationBuilder.UpdateData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTime(2023, 5, 22, 0, 46, 12, 133, DateTimeKind.Local).AddTicks(5216));
+
+            migrationBuilder.UpdateData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedAt",
+                value: new DateTime(2023, 5, 22, 0, 46, 12, 133, DateTimeKind.Local).AddTicks(5218));
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2023, 5, 22, 0, 46, 12, 133, DateTimeKind.Local).AddTicks(4951));
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTime(2023, 5, 22, 0, 46, 12, 133, DateTimeKind.Local).AddTicks(4990));
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedAt",
+                value: new DateTime(2023, 5, 22, 0, 46, 12, 133, DateTimeKind.Local).AddTicks(4992));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cities_CountryId",
+                table: "Cities",
+                column: "CountryId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cities");
+
+            migrationBuilder.UpdateData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2023, 5, 17, 22, 22, 38, 23, DateTimeKind.Local).AddTicks(4555));
+
+            migrationBuilder.UpdateData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTime(2023, 5, 17, 22, 22, 38, 23, DateTimeKind.Local).AddTicks(4557));
+
+            migrationBuilder.UpdateData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedAt",
+                value: new DateTime(2023, 5, 17, 22, 22, 38, 23, DateTimeKind.Local).AddTicks(4559));
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2023, 5, 17, 22, 22, 38, 23, DateTimeKind.Local).AddTicks(4384));
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTime(2023, 5, 17, 22, 22, 38, 23, DateTimeKind.Local).AddTicks(4400));
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedAt",
+                value: new DateTime(2023, 5, 17, 22, 22, 38, 23, DateTimeKind.Local).AddTicks(4402));
+        }
+    }
+}
